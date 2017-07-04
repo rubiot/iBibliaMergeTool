f52ba37bd5c5a1704ddc5744502091653b12b306
Rubio R. C. Terra <rubio.terra@gmail.com>
2017-07-03 22:25:57 -0300

    - Criação de filtro com regex; - Opção de selecionar campos exportados.

--- iBibliaMergeTool/Form1.Designer.cs
+++ iBibliaMergeTool/Form1.Designer.cs
@@ -28,6 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
       this.tbSrcProject = new System.Windows.Forms.TextBox();
       this.tbDstProject = new System.Windows.Forms.TextBox();
       this.label1 = new System.Windows.Forms.Label();
@@ -40,6 +41,17 @@
       this.btnPreview = new System.Windows.Forms.Button();
       this.lbRefs = new System.Windows.Forms.ListBox();
       this.btnMerge = new System.Windows.Forms.Button();
+      this.gtFilterType = new System.Windows.Forms.GroupBox();
+      this.rbAdvancedFilter = new System.Windows.Forms.RadioButton();
+      this.rbSimpleFilter = new System.Windows.Forms.RadioButton();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.cbFieldSrcText = new System.Windows.Forms.CheckBox();
+      this.cbFieldDstText = new System.Windows.Forms.CheckBox();
+      this.cbFieldPairs = new System.Windows.Forms.CheckBox();
+      this.cbFieldComments = new System.Windows.Forms.CheckBox();
+      this.cbFieldStatus = new System.Windows.Forms.CheckBox();
+      this.gtFilterType.SuspendLayout();
+      this.groupBox1.SuspendLayout();
       this.SuspendLayout();
       // 
       // tbSrcProject
@@ -47,20 +59,22 @@
       this.tbSrcProject.AllowDrop = true;
       this.tbSrcProject.Location = new System.Drawing.Point(126, 12);
       this.tbSrcProject.Name = "tbSrcProject";
-      this.tbSrcProject.Size = new System.Drawing.Size(605, 22);
+      this.tbSrcProject.ReadOnly = true;
+      this.tbSrcProject.Size = new System.Drawing.Size(542, 22);
       this.tbSrcProject.TabIndex = 0;
       // 
       // tbDstProject
       // 
-      this.tbDstProject.Location = new System.Drawing.Point(126, 59);
+      this.tbDstProject.Location = new System.Drawing.Point(126, 40);
       this.tbDstProject.Name = "tbDstProject";
-      this.tbDstProject.Size = new System.Drawing.Size(605, 22);
+      this.tbDstProject.ReadOnly = true;
+      this.tbDstProject.Size = new System.Drawing.Size(542, 22);
       this.tbDstProject.TabIndex = 0;
       // 
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(20, 12);
+      this.label1.Location = new System.Drawing.Point(23, 12);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(100, 17);
       this.label1.TabIndex = 1;
@@ -69,7 +83,7 @@
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(20, 59);
+      this.label2.Location = new System.Drawing.Point(20, 40);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(103, 17);
       this.label2.TabIndex = 2;
@@ -77,9 +91,9 @@
       // 
       // btnOpenSrcProject
       // 
-      this.btnOpenSrcProject.Location = new System.Drawing.Point(737, 11);
+      this.btnOpenSrcProject.Location = new System.Drawing.Point(674, 11);
       this.btnOpenSrcProject.Name = "btnOpenSrcProject";
-      this.btnOpenSrcProject.Size = new System.Drawing.Size(142, 23);
+      this.btnOpenSrcProject.Size = new System.Drawing.Size(205, 23);
       this.btnOpenSrcProject.TabIndex = 3;
       this.btnOpenSrcProject.Text = "Abrir...";
       this.btnOpenSrcProject.UseVisualStyleBackColor = true;
@@ -87,9 +101,9 @@
       // 
       // btnOpenDstProject
       // 
-      this.btnOpenDstProject.Location = new System.Drawing.Point(737, 59);
+      this.btnOpenDstProject.Location = new System.Drawing.Point(674, 40);
       this.btnOpenDstProject.Name = "btnOpenDstProject";
-      this.btnOpenDstProject.Size = new System.Drawing.Size(142, 23);
+      this.btnOpenDstProject.Size = new System.Drawing.Size(205, 23);
       this.btnOpenDstProject.TabIndex = 3;
       this.btnOpenDstProject.Text = "Abrir...";
       this.btnOpenDstProject.UseVisualStyleBackColor = true;
@@ -102,15 +116,16 @@
       // 
       // tbFilter
       // 
-      this.tbFilter.Location = new System.Drawing.Point(126, 106);
+      this.tbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.tbFilter.Location = new System.Drawing.Point(126, 83);
       this.tbFilter.Name = "tbFilter";
-      this.tbFilter.Size = new System.Drawing.Size(605, 22);
+      this.tbFilter.Size = new System.Drawing.Size(542, 27);
       this.tbFilter.TabIndex = 4;
       // 
       // label3
       // 
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(73, 110);
+      this.label3.Location = new System.Drawing.Point(84, 83);
       this.label3.Name = "label3";
       this.label3.Size = new System.Drawing.Size(39, 17);
       this.label3.TabIndex = 5;
@@ -118,9 +133,9 @@
       // 
       // btnPreview
       // 
-      this.btnPreview.Location = new System.Drawing.Point(738, 106);
+      this.btnPreview.Location = new System.Drawing.Point(674, 83);
       this.btnPreview.Name = "btnPreview";
-      this.btnPreview.Size = new System.Drawing.Size(142, 23);
+      this.btnPreview.Size = new System.Drawing.Size(205, 23);
       this.btnPreview.TabIndex = 6;
       this.btnPreview.Text = "Testar filtro";
       this.btnPreview.UseVisualStyleBackColor = true;
@@ -130,27 +145,132 @@
       // 
       this.lbRefs.FormattingEnabled = true;
       this.lbRefs.ItemHeight = 16;
-      this.lbRefs.Location = new System.Drawing.Point(126, 145);
+      this.lbRefs.Location = new System.Drawing.Point(126, 176);
       this.lbRefs.Name = "lbRefs";
-      this.lbRefs.Size = new System.Drawing.Size(605, 244);
+      this.lbRefs.Size = new System.Drawing.Size(542, 196);
       this.lbRefs.TabIndex = 7;
       // 
       // btnMerge
       // 
       this.btnMerge.AutoSize = true;
-      this.btnMerge.Location = new System.Drawing.Point(126, 406);
+      this.btnMerge.Location = new System.Drawing.Point(674, 345);
       this.btnMerge.Name = "btnMerge";
-      this.btnMerge.Size = new System.Drawing.Size(125, 27);
+      this.btnMerge.Size = new System.Drawing.Size(200, 27);
       this.btnMerge.TabIndex = 8;
       this.btnMerge.Text = "Mesclar projetos!";
       this.btnMerge.UseVisualStyleBackColor = true;
       this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
       // 
+      // gtFilterType
+      // 
+      this.gtFilterType.Controls.Add(this.rbAdvancedFilter);
+      this.gtFilterType.Controls.Add(this.rbSimpleFilter);
+      this.gtFilterType.Location = new System.Drawing.Point(126, 118);
+      this.gtFilterType.Name = "gtFilterType";
+      this.gtFilterType.Size = new System.Drawing.Size(542, 52);
+      this.gtFilterType.TabIndex = 9;
+      this.gtFilterType.TabStop = false;
+      this.gtFilterType.Text = "Tipo de filtro";
+      // 
+      // rbAdvancedFilter
+      // 
+      this.rbAdvancedFilter.AutoSize = true;
+      this.rbAdvancedFilter.Location = new System.Drawing.Point(254, 22);
+      this.rbAdvancedFilter.Name = "rbAdvancedFilter";
+      this.rbAdvancedFilter.Size = new System.Drawing.Size(221, 21);
+      this.rbAdvancedFilter.TabIndex = 1;
+      this.rbAdvancedFilter.Text = "Avançado (Expressão regular)";
+      this.rbAdvancedFilter.UseVisualStyleBackColor = true;
+      // 
+      // rbSimpleFilter
+      // 
+      this.rbSimpleFilter.AutoSize = true;
+      this.rbSimpleFilter.Checked = true;
+      this.rbSimpleFilter.Location = new System.Drawing.Point(15, 22);
+      this.rbSimpleFilter.Name = "rbSimpleFilter";
+      this.rbSimpleFilter.Size = new System.Drawing.Size(146, 21);
+      this.rbSimpleFilter.TabIndex = 0;
+      this.rbSimpleFilter.TabStop = true;
+      this.rbSimpleFilter.Text = "Simples (Máscara)";
+      this.rbSimpleFilter.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.cbFieldStatus);
+      this.groupBox1.Controls.Add(this.cbFieldComments);
+      this.groupBox1.Controls.Add(this.cbFieldPairs);
+      this.groupBox1.Controls.Add(this.cbFieldDstText);
+      this.groupBox1.Controls.Add(this.cbFieldSrcText);
+      this.groupBox1.Location = new System.Drawing.Point(675, 118);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(200, 212);
+      this.groupBox1.TabIndex = 10;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Campos";
+      // 
+      // cbFieldSrcText
+      // 
+      this.cbFieldSrcText.AutoSize = true;
+      this.cbFieldSrcText.Location = new System.Drawing.Point(16, 28);
+      this.cbFieldSrcText.Name = "cbFieldSrcText";
+      this.cbFieldSrcText.Size = new System.Drawing.Size(112, 21);
+      this.cbFieldSrcText.TabIndex = 0;
+      this.cbFieldSrcText.Text = "Texto origem";
+      this.cbFieldSrcText.UseVisualStyleBackColor = true;
+      // 
+      // cbFieldDstText
+      // 
+      this.cbFieldDstText.AutoSize = true;
+      this.cbFieldDstText.Location = new System.Drawing.Point(16, 63);
+      this.cbFieldDstText.Name = "cbFieldDstText";
+      this.cbFieldDstText.Size = new System.Drawing.Size(115, 21);
+      this.cbFieldDstText.TabIndex = 1;
+      this.cbFieldDstText.Text = "Texto destino";
+      this.cbFieldDstText.UseVisualStyleBackColor = true;
+      // 
+      // cbFieldPairs
+      // 
+      this.cbFieldPairs.AutoSize = true;
+      this.cbFieldPairs.Checked = true;
+      this.cbFieldPairs.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.cbFieldPairs.Location = new System.Drawing.Point(16, 99);
+      this.cbFieldPairs.Name = "cbFieldPairs";
+      this.cbFieldPairs.Size = new System.Drawing.Size(102, 21);
+      this.cbFieldPairs.TabIndex = 2;
+      this.cbFieldPairs.Text = "Associação";
+      this.cbFieldPairs.UseVisualStyleBackColor = true;
+      // 
+      // cbFieldComments
+      // 
+      this.cbFieldComments.AutoSize = true;
+      this.cbFieldComments.Checked = true;
+      this.cbFieldComments.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.cbFieldComments.Location = new System.Drawing.Point(16, 135);
+      this.cbFieldComments.Name = "cbFieldComments";
+      this.cbFieldComments.Size = new System.Drawing.Size(109, 21);
+      this.cbFieldComments.TabIndex = 3;
+      this.cbFieldComments.Text = "Comentários";
+      this.cbFieldComments.UseVisualStyleBackColor = true;
+      // 
+      // cbFieldStatus
+      // 
+      this.cbFieldStatus.AutoSize = true;
+      this.cbFieldStatus.Checked = true;
+      this.cbFieldStatus.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.cbFieldStatus.Location = new System.Drawing.Point(16, 173);
+      this.cbFieldStatus.Name = "cbFieldStatus";
+      this.cbFieldStatus.Size = new System.Drawing.Size(74, 21);
+      this.cbFieldStatus.TabIndex = 4;
+      this.cbFieldStatus.Text = "Estado";
+      this.cbFieldStatus.UseVisualStyleBackColor = true;
+      // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(891, 464);
+      this.ClientSize = new System.Drawing.Size(891, 400);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.gtFilterType);
       this.Controls.Add(this.btnMerge);
       this.Controls.Add(this.lbRefs);
       this.Controls.Add(this.btnPreview);
@@ -162,9 +282,14 @@
       this.Controls.Add(this.label1);
       this.Controls.Add(this.tbDstProject);
       this.Controls.Add(this.tbSrcProject);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "MainForm";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "iBiblia - Utilitário para mesclar projetos";
+      this.gtFilterType.ResumeLayout(false);
+      this.gtFilterType.PerformLayout();
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -184,6 +309,15 @@
     private System.Windows.Forms.Button btnPreview;
     private System.Windows.Forms.ListBox lbRefs;
     private System.Windows.Forms.Button btnMerge;
+    private System.Windows.Forms.GroupBox gtFilterType;
+    private System.Windows.Forms.RadioButton rbAdvancedFilter;
+    private System.Windows.Forms.RadioButton rbSimpleFilter;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.CheckBox cbFieldComments;
+    private System.Windows.Forms.CheckBox cbFieldPairs;
+    private System.Windows.Forms.CheckBox cbFieldDstText;
+    private System.Windows.Forms.CheckBox cbFieldSrcText;
+    private System.Windows.Forms.CheckBox cbFieldStatus;
   }
 }
 
--- iBibliaMergeTool/Form1.cs
+++ iBibliaMergeTool/Form1.cs
@@ -27,13 +27,29 @@ namespace iBibliaMergeTool
       m_dstProject.Close();
     }
 
+    private SQLiteConnection CreateConnection(string dbName)
+    {
+      SQLiteConnection conn = new SQLiteConnection($"Data Source={dbName};Version=3;UseUTF8Encoding=True;");
+
+      conn.Open();
+
+      SQLiteFunction function = new RegExSQLiteFunction();
+      var attributes = function.GetType().GetCustomAttributes(typeof(SQLiteFunctionAttribute), true).Cast<SQLiteFunctionAttribute>().ToArray();
+      if (attributes.Length == 0)
+      {
+        throw new InvalidOperationException("SQLiteFunction doesn't have SQLiteFunctionAttribute");
+      }
+      conn.BindFunction(attributes[0], function);
+
+      return conn;
+    }
+
     private void btnOpenSrcProject_Click(object sender, EventArgs e)
     {
       if (openProjectDialog.ShowDialog() == DialogResult.OK)
       {
         tbSrcProject.Text = openProjectDialog.FileName;
-        m_srcProject = new SQLiteConnection("Data Source=" + openProjectDialog.FileName + ";Version=3;UseUTF8Encoding=True;");
-        m_srcProject.Open();
+        m_srcProject = CreateConnection(openProjectDialog.FileName);
       }
     }
 
@@ -41,9 +57,8 @@ namespace iBibliaMergeTool
     {
       if (openProjectDialog.ShowDialog() == DialogResult.OK)
       {
-        m_dstProject = new SQLiteConnection("Data Source=" + openProjectDialog.FileName + ";Version=3;UseUTF8Encoding=True;");
         tbDstProject.Text = openProjectDialog.FileName;
-        m_dstProject.Open();
+        m_dstProject = CreateConnection(openProjectDialog.FileName);
       }
     }
 
@@ -61,8 +76,7 @@ namespace iBibliaMergeTool
         return;
       }
 
-      string sql = $"select pare_ref from pares where pare_ref like '{tbFilter.Text}'";
-      SQLiteCommand command = new SQLiteCommand(sql, m_srcProject);
+      SQLiteCommand command = MakeSelectStmt("pare_ref");
 
       try
       {
@@ -82,57 +96,84 @@ namespace iBibliaMergeTool
       }
     }
 
+    private SQLiteCommand MakeSelectStmt(string fields)
+    {
+      string op = rbSimpleFilter.Checked ? "LIKE" : "REGEXP";
+      string stmt = $@"SELECT {fields}
+                       FROM pares
+                       WHERE pare_ref {op} '{tbFilter.Text}'";
+
+      return new SQLiteCommand(stmt, m_srcProject);
+    }
+
+    private SQLiteCommand MakeUpdateStmt()
+    {
+      List<string> fields = new List<string>();
+
+      if (cbFieldSrcText.Checked)
+        fields.Add("pare_texto_origem = @pare_texto_origem");
+      if (cbFieldDstText.Checked)
+        fields.Add("pare_texto_destino = @pare_texto_destino");
+      if (cbFieldPairs.Checked)
+        fields.Add("pare_pares = @pare_pares");
+      if (cbFieldStatus.Checked)
+        fields.Add("pare_situacao = @pare_situacao");
+      if (cbFieldComments.Checked)
+        fields.Add("pare_comentarios = @pare_comentarios");
+
+      if (fields.Count == 0)
+        throw new Exception("Selecione ao menos um campo para ser mesclado");
+
+      string stmt = $"UPDATE pares SET { String.Join(", ", fields.ToArray()) } WHERE pare_id = @pare_id";
+
+      SQLiteCommand update = new SQLiteCommand(stmt, m_dstProject);
+
+      if (cbFieldSrcText.Checked)
+        update.Parameters.Add(new SQLiteParameter("@pare_texto_origem", DbType.String));
+      if (cbFieldDstText.Checked)
+        update.Parameters.Add(new SQLiteParameter("@pare_texto_destino", DbType.String));
+      if (cbFieldPairs.Checked)
+        update.Parameters.Add(new SQLiteParameter("@pare_pares", DbType.String));
+      if (cbFieldStatus.Checked)
+        update.Parameters.Add(new SQLiteParameter("@pare_situacao", DbType.String));
+      if (cbFieldComments.Checked)
+        update.Parameters.Add(new SQLiteParameter("@pare_comentarios", DbType.String));
+      update.Parameters.Add(new SQLiteParameter("@pare_id", DbType.String));
+
+      return update;
+    }
+
     private void btnMerge_Click(object sender, EventArgs e)
     {
       if (tbSrcProject.TextLength == 0)
       {
-        MessageBox.Show("Erro: primeiro escolha o projeto de origem!");
+        MessageBox.Show("Escolha o projeto de origem!");
         return;
       }
 
       if (tbDstProject.TextLength == 0)
       {
-        MessageBox.Show("Erro: primeiro escolha o projeto de destino!");
+        MessageBox.Show("Escolha o projeto de destino!");
         return;
       }
 
       if (tbFilter.TextLength == 0)
       {
-        MessageBox.Show("Erro: primeiro informe o filtro a ser utilizado!");
+        MessageBox.Show("Informe o filtro a ser utilizado!");
         return;
       }
- 
-      string versesSelect = $@"
-        SELECT
-          pare_id,
-          pare_ref,
-          pare_texto_origem,
-          pare_texto_destino,
-          pare_pares,
-          pare_situacao,
-          pare_comentarios
-        FROM pares
-        WHERE pare_ref like '{tbFilter.Text}'";
 
-      SQLiteCommand select = new SQLiteCommand(versesSelect, m_srcProject);
-      SQLiteDataReader reader = select.ExecuteReader();
-
-      string updateStmt = @"
-        UPDATE pares
-        SET pare_texto_origem  = @pare_texto_origem,
-            pare_texto_destino = @pare_texto_destino,
-            pare_pares         = @pare_pares,
-            pare_situacao      = @pare_situacao,
-            pare_comentarios   = @pare_comentarios
-        WHERE pare_id = @pare_id";
-
-      SQLiteCommand update = new SQLiteCommand(updateStmt, m_dstProject);
-      update.Parameters.Add(new SQLiteParameter("@pare_texto_origem", DbType.String));
-      update.Parameters.Add(new SQLiteParameter("@pare_texto_destino", DbType.String));
-      update.Parameters.Add(new SQLiteParameter("@pare_pares", DbType.String));
-      update.Parameters.Add(new SQLiteParameter("@pare_situacao", DbType.String));
-      update.Parameters.Add(new SQLiteParameter("@pare_comentarios", DbType.String));
-      update.Parameters.Add(new SQLiteParameter("@pare_id", DbType.String));
+      SQLiteCommand select = MakeSelectStmt("pare_id, pare_ref, pare_texto_origem, pare_texto_destino, pare_pares, pare_situacao, pare_comentarios");
+      SQLiteCommand update;
+      try
+      {
+        update = MakeUpdateStmt();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+        return;
+      }
 
       lbRefs.Items.Clear();
 
@@ -140,16 +181,23 @@ namespace iBibliaMergeTool
       {
         try
         {
+          SQLiteDataReader reader = select.ExecuteReader();
           while (reader.Read())
           {
             lbRefs.Items.Add($"copiando {reader["pare_ref"]}...");
 
-            update.Parameters["@pare_texto_origem"].Value  = reader["pare_texto_origem"];
-            update.Parameters["@pare_texto_destino"].Value = reader["pare_texto_destino"];
-            update.Parameters["@pare_pares"].Value         = reader["pare_pares"];
-            update.Parameters["@pare_situacao"].Value      = reader["pare_situacao"];
-            update.Parameters["@pare_comentarios"].Value   = reader["pare_comentarios"];
-            update.Parameters["@pare_id"].Value            = reader["pare_id"];
+            if (cbFieldSrcText.Checked)
+              update.Parameters["@pare_texto_origem"].Value = reader["pare_texto_origem"];
+            if (cbFieldDstText.Checked)
+              update.Parameters["@pare_texto_destino"].Value = reader["pare_texto_destino"];
+            if (cbFieldPairs.Checked)
+              update.Parameters["@pare_pares"].Value = reader["pare_pares"];
+            if (cbFieldStatus.Checked)
+              update.Parameters["@pare_situacao"].Value = reader["pare_situacao"];
+            if (cbFieldComments.Checked)
+              update.Parameters["@pare_comentarios"].Value = reader["pare_comentarios"];
+            update.Parameters["@pare_id"].Value = reader["pare_id"];
+
             update.ExecuteNonQuery();
           }
           tra.Commit();
@@ -162,6 +210,15 @@ namespace iBibliaMergeTool
           MessageBox.Show("Ocorreu um erro durante o processo, mesclagem abortada: " + ex.Message);
         }
       }
+    }
+  }
+
+  [SQLiteFunction(Name = "REGEXP", Arguments = 2, FuncType = FunctionType.Scalar)]
+  public class RegExSQLiteFunction : SQLiteFunction
+  {
+    public override object Invoke(object[] args)
+    {
+      return System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(args[1]), Convert.ToString(args[0]));
     }
   }
 }
